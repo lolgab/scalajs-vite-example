f7453938d1f1701a83c0f2dfe923619d8ad84ea6
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-02-21 22:18:34 +0100

    Update all Javascript dependencies
    Update example to latest Chart.js

--- chart/src/chart/Main.scala
+++ chart/src/chart/Main.scala
@@ -5,9 +5,27 @@ import scala.scalajs.js.annotation._
 
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
+import typings.chartJs.distTypesIndexMod.{
+  ChartComponent,
+  ChartConfiguration,
+  ChartData,
+  ChartDataset,
+  ChartOptions,
+  ChartType
+}
+import typings.chartJs.chartJsStrings._
 import typings.chartJs.mod._
 
 object Main {
+  Chart.register(
+    js.Array(
+      BarController.^,
+      BarElement.^,
+      CategoryScale.^,
+      LinearScale.^
+    ).map(_.asInstanceOf[ChartComponent])
+  )
+
   val app = div(
     canvasTag(onMountCallback { nodeCtx =>
       // Example taken from
@@ -16,51 +34,38 @@ object Main {
         .asInstanceOf[dom.html.Canvas]
         .getContext("2d")
         .asInstanceOf[dom.CanvasRenderingContext2D]
-      val chartConfiguration = ChartConfiguration()
-        .setType(ChartType.bar)
-        .setData(
-          ChartData()
-            .setLabels(
-              js.Array("Red", "Blue", "Yellow", "Green", "Purple", "Orange")
-            )
-            .setDatasets(
-              js.Array(
-                ChartDataSets()
-                  .setLabel("# of Votes")
-                  .setData(js.Array(10, 19, 3, 5, 2, 3))
-                  .setBackgroundColor(
-                    js.Array(
-                      "rgba(255, 99, 132, 0.2)",
-                      "rgba(54, 162, 235, 0.2)",
-                      "rgba(255, 206, 86, 0.2)",
-                      "rgba(75, 192, 192, 0.2)",
-                      "rgba(153, 102, 255, 0.2)",
-                      "rgba(255, 159, 64, 0.2)"
-                    )
-                  )
-                  .setBorderColor(
-                    js.Array(
-                      "rgba(255, 99, 132, 1)",
-                      "rgba(54, 162, 235, 1)",
-                      "rgba(255, 206, 86, 1)",
-                      "rgba(75, 192, 192, 1)",
-                      "rgba(153, 102, 255, 1)",
-                      "rgba(255, 159, 64, 1)"
-                    )
-                  )
-                  .setBorderWidth(1)
+      val chartConfiguration = ChartConfiguration(
+        `type` = bar,
+        data = ChartData(
+          datasets = js.Array(
+            new js.Object {
+              val label = "# of Votes"
+              val data = js.Array(10, 19, 3, 5, 2, 3)
+              val backgroundColor = js.Array(
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
               )
-            )
-        )
-        .setOptions(
-          ChartOptions().setScales(
-            ChartScales().setYAxes(
-              js.Array(CommonAxe().setTicks(TickOptions().setBeginAtZero(true)))
-            )
+              val borderColor = js.Array(
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              )
+              val borderWidth = 1
+            }.asInstanceOf[ChartDataset[bar, _]]
           )
+        ).setLabels(
+          js.Array("Red", "Blue", "Yellow", "Green", "Purple", "Orange")
         )
+      )
 
-      Chart.apply.newInstance2(ctx, chartConfiguration)
+      Chart(ctx, chartConfiguration)
     })
   )
 
